2d553de44fb0e052a1384cc010bb0fc550b95965
praakaash-jaat <jaat.praakaash@gmail.com>
2025-04-14 21:00:31 +0530

    change ollama to lmstudio

--- chatbot/chatbot/views.py
+++ chatbot/chatbot/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render,HttpResponse
 from django.http import JsonResponse
-from ollama import chat, ChatResponse
-import json
 from django.views.decorators.csrf import csrf_exempt
-import asyncio
-from . import ollama
+import lmstudio as lms
 
 def index(request):
-    return render(request, 'chatbot/index.html')
+    massage = chat_with_lms("Hello, how are you?")
+    context = {'massage': massage}
+    return render(request, 'chatbot/index.html' , context)
+
+def chat_with_lms(input_font):
+    model = lms.llm("qwen2.5-coder-3b-instruct")
+    result = model.respond("What is the meaning of life?")
+    return result
+    
+
 
