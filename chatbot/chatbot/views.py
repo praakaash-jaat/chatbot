4100d854af31497165fca8856c518bece6b2c2b3
praakaash-jaat <jaat.praakaash@gmail.com>
2025-04-14 22:53:09 +0530

    work fine

--- chatbot/chatbot/views.py
+++ chatbot/chatbot/views.py
@@ -4,15 +4,27 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 import lmstudio as lms
 
+
 def index(request):
-    output_backend = chat_with_lms("write a story?")
+    cp_request = request
+    user_input = uin_fontend(cp_request)
+    output_backend = chat_with_lms(user_input)
     output_backend = str(output_backend)
     return render(request, 'chatbot/index.html', {'message': output_backend})
+
+def uin_fontend(cp_request):
+    user_input = ""
+    if cp_request.method == 'POST':
+        user_input = cp_request.POST.get('user_input')
+        return user_input
+    return user_input
 
 def chat_with_lms(input_fontend):
     model = lms.llm("qwen2.5-coder-3b-instruct")
     output_backend = model.respond(input_fontend)
     return output_backend
+
+
     
 
 
