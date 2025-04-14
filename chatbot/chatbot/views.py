06792a005dab3f54d871973e9cd0ad0b61569bf3
praakaash-jaat <jaat.praakaash@gmail.com>
2025-04-14 22:32:49 +0530

    work more

--- chatbot/chatbot/views.py
+++ chatbot/chatbot/views.py
@@ -5,13 +5,9 @@ from django.views.decorators.csrf import csrf_exempt
 import lmstudio as lms
 
 def index(request):
-    output_backend = chat_with_lms("write a very short story?")
+    output_backend = chat_with_lms("write a story?")
     output_backend = str(output_backend)
-    output_backend = {
-        'message': output_backend,
-    }
-    output_backend = json.loads(json.dumps(output_backend))
-    return JsonResponse(output_backend)
+    return render(request, 'chatbot/index.html', {'message': output_backend})
 
 def chat_with_lms(input_fontend):
     model = lms.llm("qwen2.5-coder-3b-instruct")
