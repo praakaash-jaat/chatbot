dbf6259bdb626b533823538f6067d5bfafca9ba8
praakaash-jaat <jaat.praakaash@gmail.com>
2025-04-14 19:06:06 +0530

    css fixed

--- chatbot/chatbot/settings.py
+++ chatbot/chatbot/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,7 +120,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'chatbot/static'), # correct path
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # correct path
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # correct path
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
