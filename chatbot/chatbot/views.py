1b5c3bca5771ed50a983a4e4a3ef2b760e148d6f
praakaash-jaat <jaat.praakaash@gmail.com>
2025-04-14 21:12:36 +0530

    working model

--- chatbot/chatbot/views.py
+++ chatbot/chatbot/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render,HttpResponse
 from django.http import JsonResponse
+import json
 from django.views.decorators.csrf import csrf_exempt
 import lmstudio as lms
 
 def index(request):
-    massage = chat_with_lms("Hello, how are you?")
-    context = {'massage': massage}
-    return render(request, 'chatbot/index.html' , context)
+    output_back = chat_with_lms("Hello, how are you?")
+    data = {'message': output_back}
+    data = str(data)
+    data = json.loads(data)
+    return HttpResponse(data)
 
 def chat_with_lms(input_font):
     model = lms.llm("qwen2.5-coder-3b-instruct")
